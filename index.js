f10c4e93a04882e0d93c0a56ca7bce40a3d76e6a
Abdel <chamati@live.com>
2022-01-16 01:01:36 +0100

    Display in CSS

---
+++ index.js
@@ -0,0 +1,13 @@
+// JavaScript Documentconst articles = document.querySelectorAll('.container');
+const articles = document.querySelectorAll('.container');
+const select = document.querySelector('select');
+
+function updateDisplay() {
+  articles.forEach((article) => {
+    article.style.display = select.value;
+  });
+}
+
+select.addEventListener('change', updateDisplay);
+
+updateDisplay();
